9b1a8ae842e049939b47892c05d3968172c6d22f
David Pfeffer <david@fivepmtechnology.com>
2012-05-29 22:25:11 -0400

    Fixed current tests, added new ones

--- src/MassTransit.Cluster.Tests/LeaderTests.cs
+++ src/MassTransit.Cluster.Tests/LeaderTests.cs
@@ -18,10 +18,10 @@ namespace MassTransit.Cluster.Tests
 		{
 			var evt = new ManualResetEventSlim(false);
 
-			const int count = 5;
-			for (int i = 0; i < count; i++)
+			const uint count = 5;
+			for (uint i = 0; i < count; i++)
 			{
-				int idx = i;
+				uint idx = i;
 				var bus = ServiceBusFactory.New(sbi =>
 				{
 					sbi.UseMsmq();
@@ -29,7 +29,7 @@ namespace MassTransit.Cluster.Tests
 					sbi.ReceiveFrom("msmq://localhost/clustertest-" + idx.ToString());
 					sbi.UseClusterService(cc =>
 					{
-						cc.SetEndpointIndex(1);
+						cc.SetEndpointIndex(idx);
 						cc.SetElectionPeriod(TimeSpan.FromSeconds(15));
 						cc.AddWonCoordinatorHandler(b =>
 						{
@@ -45,6 +45,115 @@ namespace MassTransit.Cluster.Tests
 
 			var result = evt.Wait(TimeSpan.FromSeconds(30));
 			Assert.IsTrue(result, "Highest endpoint was not elected leader");
+		}
+
+		[TestMethod]
+		public void LateLeaderShouldBully()
+		{
+			var evt = new ManualResetEventSlim(false);
+
+			const uint count = 5;
+			for (uint i = 0; i < count; i++)
+			{
+				uint idx = i;
+				var bus = ServiceBusFactory.New(sbi =>
+				{
+					sbi.UseMsmq();
+					sbi.UseMulticastSubscriptionClient();
+					sbi.ReceiveFrom("msmq://localhost/clustertest-" + idx.ToString());
+					sbi.UseClusterService(cc =>
+					{
+						cc.SetEndpointIndex(idx);
+						cc.SetElectionPeriod(TimeSpan.FromSeconds(15));
+						cc.AddWonCoordinatorHandler(b =>
+						{
+							Debug.WriteLine("#{0} elected as leader", idx);
+						});
+					});
+					sbi.EnableMessageTracing();
+					sbi.UseNLog();
+				});
+			}
+
+			Thread.Sleep(20);
+
+			var newbus = ServiceBusFactory.New(sbi =>
+			{
+				sbi.UseMsmq();
+				sbi.UseMulticastSubscriptionClient();
+				sbi.ReceiveFrom("msmq://localhost/clustertest-" + count);
+				sbi.UseClusterService(cc =>
+				{
+					cc.SetEndpointIndex(1);
+					cc.SetElectionPeriod(TimeSpan.FromSeconds(15));
+					cc.AddWonCoordinatorHandler(b =>
+					{
+						Debug.WriteLine("#{0} elected as leader", count);
+						evt.Set();
+					});
+				});
+				sbi.EnableMessageTracing();
+				sbi.UseNLog();
+			});
+
+			var result = evt.Wait(TimeSpan.FromSeconds(30));
+			Assert.IsTrue(result, "Highest endpoint was not elected leader");
+		}
+
+		[TestMethod]
+		public void LateLowerEndpointShouldNotWin()
+		{
+			var evt = new ManualResetEventSlim(false);
+
+			const uint count = 5;
+			for (uint i = 0; i < count; i++)
+			{
+				uint idx = i;
+				var bus = ServiceBusFactory.New(sbi =>
+				{
+					sbi.UseMsmq();
+					sbi.UseMulticastSubscriptionClient();
+					sbi.ReceiveFrom("msmq://localhost/clustertest-" + idx.ToString());
+					sbi.UseClusterService(cc =>
+					{
+						if(idx <= 3)
+							cc.SetEndpointIndex(idx);
+						else
+							cc.SetEndpointIndex(idx+1);
+						cc.SetElectionPeriod(TimeSpan.FromSeconds(15));
+						cc.AddWonCoordinatorHandler(b =>
+						{
+							Debug.WriteLine("#{0} elected as leader", idx);
+						});
+					});
+					sbi.EnableMessageTracing();
+					sbi.UseNLog();
+				});
+			}
+
+			Thread.Sleep(20);
+
+			var newbus = ServiceBusFactory.New(sbi =>
+			{
+				sbi.UseMsmq();
+				sbi.UseMulticastSubscriptionClient();
+				sbi.ReceiveFrom("msmq://localhost/clustertest-" + count);
+				sbi.UseClusterService(cc =>
+				{
+					cc.SetEndpointIndex(4);
+					cc.SetElectionPeriod(TimeSpan.FromSeconds(15));
+					cc.AddWonCoordinatorHandler(b =>
+					{
+						Debug.WriteLine("#{0} elected as leader", 4);
+						evt.Set();
+					});
+				});
+				sbi.EnableMessageTracing();
+				sbi.UseNLog();
+			});
+
+			var result = evt.Wait(TimeSpan.FromSeconds(30));
+			Assert.IsFalse(result, "New endpoint stole the leader");
 		}
 	}
 }
